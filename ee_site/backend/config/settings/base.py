7a0e370d5d3200ca49a3c5e3cf49c3d0b6ec4acd
Armen Ambartsumyan <armen0858@gmail.com>
2026-04-28 20:02:05 +0300

    Setup PostgreSQL and database foundation for EE backend
    - installed and configured PostgreSQL 16
    - created ee_platform_db database
    - created dedicated ee_user database user
    - connected Django to PostgreSQL
    - installed psycopg
    - migrated from sqlite to PostgreSQL
    - configured DATABASES via .env
    - added requirements structure
    - applied initial Django migrations

--- ee_site/backend/config/settings/base.py
+++ ee_site/backend/config/settings/base.py
@@ -22,6 +22,8 @@ ALLOWED_HOSTS = os.getenv(
 ).split(",")
 
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -68,9 +70,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
